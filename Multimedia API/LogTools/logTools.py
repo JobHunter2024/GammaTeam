6c3ce541f91a6f8a0fd49cd0d555bc225ab02bb1
andrei.habas <habasescuandrei1234@gmail.com>
2024-10-26 03:37:17 +0300

    Project Logging Test

--- Multimedia API/LogTools/logTools.py
+++ Multimedia API/LogTools/logTools.py
@@ -9,10 +9,18 @@ load_dotenv(dotenv_path=dotenv_path)
 PAPERTRAIL_HOST = os.getenv("PAPERTRAIL_HOST")
 PAPERTRAIL_PORT = int(os.getenv("PAPERTRAIL_PORT"))
 
-def logInfo(message) -> None :
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    handler = SysLogHandler(address=(PAPERTRAIL_HOST,PAPERTRAIL_PORT))
-    logger.addHandler(handler)
+logger = logging.getLogger("infoLog")
+logger.setLevel(logging.INFO)
+handler = SysLogHandler(address=(PAPERTRAIL_HOST,PAPERTRAIL_PORT))
+formatter = logging.Formatter('%(asctime)s Logging@Module Appname : %(levelname)s[%(name)s] %(message)s', datefmt='%b %d %H:%M:%S')
 
-    logger.debug(message)
+handler.setLevel(logging.INFO)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+def logInfo(message) -> None :
+    logger.info(message)
+
+if __name__ == '__main__':
+    logInfo("Hellow")
+    print("Hello")
--- Multimedia API/app.py
+++ Multimedia API/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from LogTools import logTools
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
+    logTools.logInfo("Hello World!")
     return 'Hello World!'
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
