c4b37e338bd56b4225b5d0f7b68e93e5b26f5194
andrei.habas <habasescuandrei1234@gmail.com>
2024-11-10 16:31:54 +0200

    updated

--- Multimedia API/LogTools/logTools.py
+++ Multimedia API/LogTools/logTools.py
@@ -12,7 +12,7 @@ PAPERTRAIL_PORT = int(os.getenv("PAPERTRAIL_PORT"))
 logger = logging.getLogger("infoLog")
 logger.setLevel(logging.INFO)
 handler = SysLogHandler(address=(PAPERTRAIL_HOST,PAPERTRAIL_PORT))
-formatter = logging.Formatter('%(asctime)s Logging@Module Appname : %(levelname)s[%(name)s] %(message)s', datefmt='%b %d %H:%M:%S')
+formatter = logging.Formatter('%(asctime)s Logging@Module MultimediaAPI : %(levelname)s[%(name)s] %(message)s', datefmt='%b %d %H:%M:%S')
 
 handler.setLevel(logging.INFO)
 handler.setFormatter(formatter)
@@ -22,5 +22,5 @@ def logInfo(message) -> None :
     logger.info(message)
 
 if __name__ == '__main__':
-    logInfo("Hellow")
+    logInfo("Testing logging from python")
     print("Hello")
---
+++ QueryingAPI/Querying API/src/main/java/com/jobhunter24/queryingapi/aspects/LoggingAspect.java
@@ -0,0 +1,4 @@
+package com.jobhunter24.queryingapi.aspects;
+
+public class LoggingAspect {
+}
